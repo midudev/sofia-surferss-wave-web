c1acd18ff069492a92b953d05cc539a6fe50ac61
midudev <miduga@gmail.com>
2025-08-06 16:44:55 +0200

    Fix missing config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.surferswavetrip.com',
+
   vite: {
     plugins: [tailwindcss()],
   },
