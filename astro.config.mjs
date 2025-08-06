d09f183176ba4459a2b9df911f8d497cc603aa3a
midudev <miduga@gmail.com>
2025-08-06 16:43:46 +0200

    Upgrade dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   build: {
     inlineStylesheets: 'always',
   },
+
+  integrations: [sitemap()],
 })
